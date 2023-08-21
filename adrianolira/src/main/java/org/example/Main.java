6522fa9c4c2d78fb1548e5aa178feb8e5efe4d53
AdrianoLira <adriano.lira@cyan-agro.com>
2023-08-21 20:07:17 -0300

    Add call methods on Main class

--- adrianolira/src/main/java/org/example/Main.java
+++ adrianolira/src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TaskManager taskManager = new TaskManager();
+
+        taskManager.createTask(1, "Buy groceries", "Milk, eggs, bread", "2023-08-25", Priority.MEDIUM);
+        taskManager.createTask(2, "Finish project", "Complete documentation", "2023-09-10", Priority.HIGH);
+        taskManager.createTask(3, "Go for a run", "Morning jog", "2023-08-22", Priority.LOW);
+
+        System.out.println("All tasks:");
+        List<Task> allTasks = taskManager.getAllTasks();
+        for (int i = 0; i < allTasks.size(); i++) {
+            System.out.println("Task " + (i + 1) + ":\n" + allTasks.get(i));
+        }
+
+        taskManager.updateTask(1, "Finish project", "Complete coding", "2023-09-15", Priority.HIGH);
+        taskManager.deleteTask(2);
+
+        System.out.println("\nUpdated tasks:");
+        allTasks = taskManager.getAllTasks();
+        for (int i = 0; i < allTasks.size(); i++) {
+            System.out.println("Task " + (i + 1) + ":\n" + allTasks.get(i));
+        }
+
     }
 }
