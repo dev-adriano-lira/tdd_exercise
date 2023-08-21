29b9bf25864bbbe362a705aa8f667c168064759a
AdrianoLira <adriano.lira@cyan-agro.com>
2023-08-21 18:39:21 -0300

    Adjust TaskManagerTest

--- adrianolira/src/test/java/TaskManagerTest.java
+++ adrianolira/src/test/java/TaskManagerTest.java
@@ -1,5 +1,9 @@
+import org.example.Priority;
+import org.example.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
